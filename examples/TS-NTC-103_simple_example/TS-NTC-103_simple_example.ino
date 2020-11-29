b58077caf1ddf044b75ddb9309e08f1037675cd0
michaelwilkinson1992 <44751666+michaelwilkinson1992@users.noreply.github.com>
2020-11-29 13:01:48 +0100

    Update TS-NTC-103_simple_example.ino

--- examples/TS-NTC-103_simple_example/TS-NTC-103_simple_example.ino
+++ examples/TS-NTC-103_simple_example/TS-NTC-103_simple_example.ino
@@ -6,7 +6,7 @@ void setup() {
 
 void loop() {
   TS_NTC_103 sensor; //start an instance of the library
-  //TS_NTC_103 sensor(3.3,12);support for 3.3 volt board and/or 12bit analog read resolution
+  //TS_NTC_103 sensor(3.3,12); //support for 3.3 volt board and/or 12bit analog read resolution
   Serial.print("temp: ");
   Serial.println(sensor.getTemp(analogRead(0)));
   delay(1000);
